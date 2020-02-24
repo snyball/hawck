3dd98ba4f2505a3ffccc212ef087d53f6818ea3e
Jonas Møller <jonas.moller@student.uib.no>
2020-02-24 21:02:00 +0100

    Fixed KBDB indentation.

--- src/KBDB.cpp
+++ src/KBDB.cpp
@@ -1,81 +1,79 @@
 #include "KBDB.hpp"
 #include "utils.hpp"
 
-#include <iostream>
-#include <fstream>
 #include <filesystem>
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 namespace fs = std::filesystem;
 
 // As of 2020-02-22 input_id is a vector of 4 __u16 integers.
 // We make assumptions based on that.
-static_assert(is_same<decltype(((struct input_id*) nullptr)->bustype),
-                      decltype(((struct input_id*) nullptr)->vendor)>::value);
-static_assert(is_same<decltype(((struct input_id*) nullptr)->vendor),
-                      decltype(((struct input_id*) nullptr)->product)>::value);
-static_assert(is_same<decltype(((struct input_id*) nullptr)->product),
-                      decltype(((struct input_id*) nullptr)->version)>::value);
+static_assert(is_same<decltype(((struct input_id *)nullptr)->bustype),
+                      decltype(((struct input_id *)nullptr)->vendor)>::value);
+static_assert(is_same<decltype(((struct input_id *)nullptr)->vendor),
+                      decltype(((struct input_id *)nullptr)->product)>::value);
+static_assert(is_same<decltype(((struct input_id *)nullptr)->product),
+                      decltype(((struct input_id *)nullptr)->version)>::value);
 
 /**
  * For unprivileged access we can't use ioctl()s, but sysfs is world-readable so
  * we can pull all the data from there.
  */
 KBDInfo::KBDInfo(const struct input_id *id) : id(*id) {
-  for (auto d : fs::directory_iterator("/sys/class/input")) {
-    string path = d.path();
-    string name = pathBasename(path);
-    if (d.is_directory() && stringStartsWith(name, "event")) {
-      auto id_path = pathJoin(path, "device", "id");
-      struct input_id cid;
-      typedef decltype(cid.vendor) id_t;
-      vector<pair<string, id_t *>> parts = {{"bustype", &cid.bustype},
-                                            {"vendor", &cid.vendor},
-                                            {"product", &cid.product},
-                                            {"version", &cid.version}};
+    for (auto d : fs::directory_iterator("/sys/class/input")) {
+        string path = d.path();
+        string name = pathBasename(path);
+        if (d.is_directory() && stringStartsWith(name, "event")) {
+            auto id_path = pathJoin(path, "device", "id");
+            struct input_id cid;
+            typedef decltype(cid.vendor) id_t;
+            vector<pair<string, id_t *>> parts = {{"bustype", &cid.bustype},
+                                                  {"vendor", &cid.vendor},
+                                                  {"product", &cid.product},
+                                                  {"version", &cid.version}};
 
-      // Read parts of the id
-      for (auto [part, ptr] : parts) {
-        ifstream idp(pathJoin(id_path, part));
-        if (!idp.is_open())
-          throw SystemError("Unable to read: " + pathJoin(id_path, part));
-        idp >> hex >> *ptr;
-      }
+            // Read parts of the id
+            for (auto [part, ptr] : parts) {
+                ifstream idp(pathJoin(id_path, part));
+                if (!idp.is_open())
+                    throw SystemError("Unable to read: " + pathJoin(id_path, part));
+                idp >> hex >> *ptr;
+            }
 
-      if (!::memcmp(&cid, id, sizeof(cid))) {
-        // Found match
-        initFrom(pathJoin(path, "device"));
-      }
+            if (!::memcmp(&cid, id, sizeof(cid))) {
+                // Found match
+                initFrom(pathJoin(path, "device"));
+            }
+        }
     }
-  }
 }
 
 std::string KBDInfo::getID() noexcept {
-  std::stringstream ss;
-  ss << id.vendor << ":" << id.product << ":";
-  for (char c : name) {
-    if (c == ' ')
-      ss << '_';
-    else
-      ss << c;
-  }
-  return ss.str();
+    std::stringstream ss;
+    ss << id.vendor << ":" << id.product << ":";
+    for (char c : name) {
+        if (c == ' ')
+            ss << '_';
+        else
+            ss << c;
+    }
+    return ss.str();
 }
 
-void KBDInfo::initFrom(const std::string& path) noexcept(false) {
-  vector<pair<string, string *>> parts = {{"name", &name},
-                                          {"phys", &phys},
-                                          {"uevent", &uevent},
-                                          {"device/uevent", &dev_uevent}};
+void KBDInfo::initFrom(const std::string &path) noexcept(false) {
+    vector<pair<string, string *>> parts = {
+        {"name", &name}, {"phys", &phys}, {"uevent", &uevent}, {"device/uevent", &dev_uevent}};
 
-  for (auto [name, ptr] : parts) {
-    ifstream in(pathJoin(path, name));
-    if (!in.is_open())
-      throw SystemError("Unable to read: " + pathJoin(path, name));
-    getline(in, *ptr);
-  }
+    for (auto [name, ptr] : parts) {
+        ifstream in(pathJoin(path, name));
+        if (!in.is_open())
+            throw SystemError("Unable to read: " + pathJoin(path, name));
+        getline(in, *ptr);
+    }
 
-  drv = pathBasename(realpath_safe(pathJoin(path, "device", "driver")));
+    drv = pathBasename(realpath_safe(pathJoin(path, "device", "driver")));
 }
 
 KBDB::KBDB() {}
