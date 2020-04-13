52850302f70811a4e9ab16c547c7bc36e15b844a
Jonas Møller <jonas.moller@student.uib.no>
2020-04-13 15:34:44 +0200

    Add install script.

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+##
+## No-frills install script for all the distributions that don't have a Hawck
+## package.
+##
+
+## DWIM if the user decided to run the install script with sudo.
+sudo () { command sudo "$@"; }
+sudown () { "$@"; }
+if [ $(whoami) = root ]; then
+    sudo () { "$@"; }
+    sudown () { su -s /bin/bash "$(logname)" -c "$@"; }
+fi
+
+install () {
+    source ../bin/hawck-git/hawck-git.install
+    ninja install
+    post_install
+}
+
+mkdir -p build || exit 1
+pushd build
+meson .. || exit 1
+ninja || exit 1
+sudo bash -c "$(declare -pf install); install" || exit 1
+popd
+sudown bin/hawck-user-setup.sh
+echo "Done."
