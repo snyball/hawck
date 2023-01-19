d5d0d60bd12d88d13f75ec8b372746d1c469e973
Edward O <edoubrayrie@gmail.com>
2023-01-19 23:10:51 +0100

    let 'replace' remap keys even with modifers

--- src/Lua/Hawck.lua
+++ src/Lua/Hawck.lua
@@ -1,18 +1,18 @@
 --[====================================================================================[
    Hawck main library.
-   
+
    Copyright (C) 2018 Jonas Møller (no) <jonasmo441@gmail.com>
    All rights reserved.
-   
+
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are met:
-   
+
    1. Redistributions of source code must retain the above copyright notice, this
       list of conditions and the following disclaimer.
    2. Redistributions in binary form must reproduce the above copyright notice,
       this list of conditions and the following disclaimer in the documentation
       and/or other materials provided with the distribution.
-   
+
    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -115,13 +115,11 @@ insert = LazyF.new(function (str)
 end)
 
 replace = LazyF.new(function (key)
-    kbd:withCleanMods(function ()
         if kbd:hadKeyDown() then
           kbd:down(key)
         elseif kbd:hadKeyUp() then
           kbd:up(key)
         end
-    end)
 end)
 sub = replace
 
